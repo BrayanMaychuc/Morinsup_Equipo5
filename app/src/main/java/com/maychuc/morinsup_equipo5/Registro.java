97ffdaadf8680ea02bcbdc23271e7a1f5eaad0c8
BrayanMaychuc <brayanmay137@gmail.com>
2022-02-24 16:48:32 -0600

    Lógica de los botones

--- app/src/main/java/com/maychuc/morinsup_equipo5/Registro.java
+++ app/src/main/java/com/maychuc/morinsup_equipo5/Registro.java
@@ -1,12 +1,18 @@
 package com.maychuc.morinsup_equipo5;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Registro extends AppCompatActivity {
+
+    private EditText txtusuario, txtPass, txtpass2;
+    private Button btnregitrar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
