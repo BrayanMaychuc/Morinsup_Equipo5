e99eb74d9335f51ff82dcebf5e10eec36c84e0a3
BrayanMaychuc <brayanmay137@gmail.com>
2022-02-24 19:49:37 -0600

    Validar
    Se realizo la lógica para validar y crear usuarios, de igual forma, se agrego las funciones para pasar a la página siguiente

--- app/src/main/java/com/maychuc/morinsup_equipo5/MainActivity.java
+++ app/src/main/java/com/maychuc/morinsup_equipo5/MainActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etxtUsuario,etxtPass;
     private Button btnIniciar, btnRegistrar;
+    private Credenciales archivo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +33,22 @@ public class MainActivity extends AppCompatActivity {
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(etxtUsuario.getText().toString().isEmpty() |
+                        etxtPass.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"No deje campos vacíos",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if
+                (archivo.getValidacion(etxtUsuario.getText().toString(),etxtPass.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),"Usuario válido", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),archivo.getEncriptar(etxtPass.getText().toString()),Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(getApplicationContext(), PaginaPrincipal.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(),"El usuario no está registrado",Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
     }
 }
--- app/src/main/java/com/maychuc/morinsup_equipo5/Registro.java
+++ app/src/main/java/com/maychuc/morinsup_equipo5/Registro.java
@@ -1,16 +1,22 @@
 package com.maychuc.morinsup_equipo5;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class Registro extends AppCompatActivity {
 
     private EditText txtusuario, txtPass, txtpass2;
     private Button btnregitrar;
     private int PERMISO_REQUERIDO=11;
+    private static Credenciales archivo;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,53 @@ public class Registro extends AppCompatActivity {
         txtPass.findViewById(R.id.txtPass);
         txtpass2.findViewById(R.id.txtpass2);
         btnregitrar.findViewById(R.id.btnguardar);
+
+        archivo = new Credenciales(getApplicationContext().getExternalFilesDir(null).getPath());
+
+        btnregitrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtPass.getText().toString().isEmpty() |
+                        txtpass2.getText().toString().isEmpty() || txtusuario.getText().toString().isEmpty
+                        ()) {
+                    Toast.makeText(getApplicationContext(), "No deje campos vacíos",
+                            Toast.LENGTH_SHORT).show();
+                } else if
+                (archivo.getValidaPassUs(txtPass.getText().toString().trim(), txtpass2.getText().
+                                toString().trim()) == false) {
+                    Toast.makeText(getApplicationContext(), "Las contraseñas son diferentes",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (archivo.getDuplicado(txtusuario.getText().toString()) == true) {
+                    Toast.makeText(getApplicationContext(), "El usuario se encuentra en la BD",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    int resultado =
+                            archivo.setInsertarCredenciales(txtusuario.getText().toString(), txtPass.getText(
+                            ).toString(), getApplicationContext());
+                    if (resultado == 1) {
+                        finish();
+                        Toast.makeText(getApplicationContext(), "El usuario se registró con éxito", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
+    }
+
+    public void setPermiso() {
+        int permisosd = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permisosdescribir = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permisosd != getPackageManager().PERMISSION_GRANTED || permisosdescribir
+                != getPackageManager().PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new
+                        String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISO_REQUERIDO);
+            }
+        }
+
+
 
     }
 }
