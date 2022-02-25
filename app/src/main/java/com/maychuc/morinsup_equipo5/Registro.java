391ac48bd21b67f794cb50aa6c7ff9409001733f
BrayanMaychuc <brayanmay137@gmail.com>
2022-02-24 19:22:26 -0600

    Registro y Credenciales
    Realizando la lógica de registro y aplicando las funciones para credenciales

--- app/src/main/java/com/maychuc/morinsup_equipo5/Registro.java
+++ app/src/main/java/com/maychuc/morinsup_equipo5/Registro.java
@@ -10,9 +10,17 @@ public class Registro extends AppCompatActivity {
 
     private EditText txtusuario, txtPass, txtpass2;
     private Button btnregitrar;
+    private int PERMISO_REQUERIDO=11;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Estas variables van a poder leer los datos que les insertemos
+        //en el caso del boton, permitira la funcion click
+        txtusuario.findViewById(R.id.txtUs);
+        txtPass.findViewById(R.id.txtPass);
+        txtpass2.findViewById(R.id.txtpass2);
+        btnregitrar.findViewById(R.id.btnguardar);
 
     }
 }
